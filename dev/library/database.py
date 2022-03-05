4e284fad2fb7c16abc434e883a94669915809db2
Eric Turner <me@ericturner.it>
2022-03-05 10:49:37 -0500

    database loaded!

--- dev/library/database.py
+++ dev/library/database.py
@@ -1,18 +1,58 @@
-def fetch_page_title(page):
-    opt1 = page.get('properties', {}).get('title', {}).get('title', [{}])[0].get('plain_text')
-    opt2 = page.get('title', [{}])[0].get('plain_text')
-    opt3 = fetch_page_title_db(page)
+from pprint import pprint
 
-    return opt1 or opt2 or opt3 or '!!!!!!!!!!!!!! ERR !!!!!!!!!!!!!!'
+def fetch_page_title(page):
+    opt1 = None
+    opt2 = None
+    opt3 = None
+
+    # pages may not actually have a title (like db items)
+    # check if the elements even exist to pull them
+    if page.get('properties', {}).get('title', {}).get('title', [{}]):
+        opt1 = page.get('properties', {}).get('title', {}).get('title', [{}])[0].get('plain_text')
+    if page.get('title'):
+        opt2 = page.get('title', [{}])[0].get('plain_text')
+    if opt1 == None and opt2 == None:
+        opt3 = fetch_page_title_db(page)
+    return opt1 or opt2 or opt3 or ''
 
 # the third type will be nested in a property, for a database item
 # problem is, the property can be named anything, we need to find the id="title"
 def fetch_page_title_db(page):
-    try:
-        for property in page['properties']:
-            for subprop in property:
-                for id, val in subprop.items():
-                    if id == 'id' and val == 'title':
-                        return subprop.get('title', [{}])[0].get('plain_text')                
-    except: 
-        return None
+    for p_key, p_val in page['properties'].items():
+        if p_val['id'] == 'title':
+            # even if it has a "id":"title" dict, still may end up having no page title for the db item
+            if len(p_val.get('title')) >= 1:
+                return p_val.get('title', [{}])[0].get('plain_text') 
+            else:
+                return ""               
+
+def create_db(db, pages):
+    print('* No database found! Creating notion.db...')
+    with db:
+        db.execute("""
+            CREATE TABLE PAGE (
+                id VARCHAR(40),
+                title VARCHAR(100),
+                created_time DATETIME,
+                last_edited_time DATETIME,
+                type VARCHAR(10),
+                url VARCHAR(90)
+            );
+        """)
+        db.execute("""
+            CREATE TABLE PAGE_PARENT (
+                id VARCHAR(40),
+                parent_id VARCHAR(40),
+                parent_type VARCHAR(15)
+            );
+        """)
+    print('* Schema loaded. Populating data from db.json (this may take awhile)...')
+        # iterate over the pages and load them into the db
+
+    for page in pages:
+        #from pprint import pprint
+        #pprint(page)
+        p_id = page['id']
+        # title can be in one of several places depending on page type
+        title = fetch_page_title(page)
+        print('** Importing {} - {}'.format(p_id, title))
--- dev/test.py
+++ dev/test.py
@@ -4,7 +4,7 @@ import json
 import os
 # my functions, in separate files to keep this one cleaner
 from library.download import get_all_pages, save_db_json
-from library.database import fetch_page_title
+from library.database import create_db
 
 # why not
 print(
@@ -53,38 +53,17 @@ else:
 
 
 # Stage 2: JSON to Sqlite3
-db_prexisting = True if os.path.exists('./notion.db') else False
+if os.path.exists('./notion.db'):
+    val = input('* notion.db already exists! Do you want to recreate it? (y/n)')
+    # download a fresh copy
+    # not even gonna input validate. If 'y' isn't explicity passed then just move on.
+    if val=='y':
+        print('* Generating new notion.db...')
+        os.remove('notion.db')          # removing it first causes sqlite3 to regenerate a new one
+        con = sl.connect('./notion.db') 
+        create_db(con, pages) 
+    else:
+        print('* Loading existing notion.db...')
 
-con = sl.connect('./notion.db') 
-# if the db file didn't already exist, let's load our .json
-if not db_prexisting:
-    print('* No database found! Creating notion.db...')
-    with con:
-        con.execute("""
-            CREATE TABLE PAGE (
-                id VARCHAR(40),
-                title VARCHAR(100),
-                created_time DATETIME,
-                last_edited_time DATETIME,
-                type VARCHAR(10),
-                url VARCHAR(90)
-            );
-        """)
-        con.execute("""
-            CREATE TABLE PAGE_PARENT (
-                id VARCHAR(40),
-                parent_id VARCHAR(40),
-                parent_type VARCHAR(15)
-            );
-        """)
-    print('* Schema loaded. Populating data from db.json (this may take awhile)...')
-    
-    # iterate over the pages and load them into the db
 
-    for page in pages:
-        #from pprint import pprint
-        #pprint(page)
-        p_id = page['id']
-        # title can be in one of several places depending on page type
-        title = fetch_page_title(page)
-        print('** Importing {} - {}'.format(p_id, title))
+
